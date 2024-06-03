f05fd376d8fb0023a7806f332136776d2ac0c1c4
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-04 02:54:49 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
 const title = "Transaction Farcaster Frame on OP";
-const frameImgUrl = `${HOST_URL}/NFTImage.png`;
+const frameImgUrl = `${HOST_URL}/nft-1.jpg`;
 
 export default function Home() {
   return (
